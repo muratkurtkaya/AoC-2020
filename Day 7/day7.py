4da2e475ebd2d93cdd36ae4926d9ebde80487589
Murat Kurtkaya <muratkurtkayaa@gmail.com>
2021-04-19 20:54:59 +0300

    first commit

---
+++ Day 7/day7.py
@@ -0,0 +1,33 @@
+with open("bags.txt") as f:
+	bags = f.read().splitlines()
+
+print("shiny coral bag" in bags[0])
+
+
+
+indx = bags[0].index("contain")
+#print(bags[0][:indx-5])
+def solv1(bags):
+	direct_bags = []
+
+	for bag in bags: 
+		indx = bag.index("contain")
+		if "shiny gold" in bag[indx:]:
+			direct_bags.append(bag[:indx].rstrip("bags "))
+
+	for dir_bag in direct_bags:
+		for bag in bags:
+			indx = bag.index("contain")
+			cand_bag = bag[:indx].rstrip("bags ")
+			if dir_bag in bag[indx:] and bag[:indx].rstrip("bags ") not in direct_bags:
+				direct_bags.append(cand_bag)
+	return direct_bags
+
+# def count(bags,bag,sub_count):
+	
+
+
+
+ans = len(solv1(bags))
+print(ans)
+#print(direct_bags)
