2ce3cbc65c40493ae9c090f92d29e8b217690e08
Murat Kurtkaya <muratkurtkayaa@gmail.com>
2021-04-24 18:05:35 +0300

    applied chinese division theorem

--- Day 13/day 13_2.py
+++ Day 13/day 13_2.py
@@ -1,15 +1,18 @@
 with open("data.txt") as f:
 	data = f.read().splitlines()
 
-busses = []
+busses = ["x" if i =="x" else int(i) for i in data[1].split(",")] #ignore first line
 
-for i,dat in enumerate(data[1].split(",")):
-	if dat != "x":
-		busses.append((int(dat),i))
-busses.sort(reverse=True)
+# chinese division theorem
+def solv2(busses):
+    mods = {bus: -i % bus for i, bus in enumerate(busses) if bus != "x"}
+    vals = list(mods)
+    ans = mods[vals[0]]
+    inc = vals[0]
+    for i in vals[1:]:
+        while ans % i != mods[i]:
+            ans += inc
+        inc *= i
+    return ans
 
-print(busses)
-
-#Brute force?
-
-
+print("ans2:",solv2(busses))
